e67b8f646bfa9afd0d6b791fa8a1f6dff5443358
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-03-04 12:34:33 +0200

    Initial commit

---
+++ XIBCustomView/CustomView.swift
@@ -0,0 +1,31 @@
+//
+//  CustomView.swift
+//  XIBCustomView
+//
+//  Created by Ahmad Hemeda on 04/03/2024.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var label: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+    
+    private func configureView() {
+        Bundle.main.loadNibNamed("CustomView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
--- XIBCustomView/ViewController.swift
+++ XIBCustomView/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        stackView.distribution = .fillEqually
+        
+        let firstCustomView = CustomView()
+        firstCustomView.label.text = "First CustomView's Label"
+        firstCustomView.contentView.backgroundColor = .green
+        stackView.addArrangedSubview(firstCustomView)
+
+        let secondCustomView = CustomView()
+        secondCustomView.label.text = "Second CustomView's Label"
+        secondCustomView.contentView.backgroundColor = .blue
+        stackView.addArrangedSubview(secondCustomView)
+
+        let thirdCustomView = CustomView()
+        thirdCustomView.label.text = "Third CustomView's Label"
+        thirdCustomView.contentView.backgroundColor = .yellow
+        stackView.addArrangedSubview(thirdCustomView)
+
     }
-
-
 }
 
